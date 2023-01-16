048e6f8c2ea82b7237631ee7078d18d485cb369b
Hurma12253 <slava1511200@gmail.com>
2023-01-16 11:09:39 +0500

    fix: change aspect ration of image

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -20,17 +20,17 @@ const Card: React.FC<ICardProps> = ({
 				/>
 			</div>
 			<div className="card__info">
-				<div className="card__title">{title}</div>
+				<div className="card__title cut-text">{title}</div>
 				<div className="card__info-list">
-					<div className="card__row">
+					<div className="card__row cut-text">
 						<div className="card__parameter">Author:</div>
 						<div className="card__value">{author}</div>
 					</div>
-					<div className="card__row">
+					<div className="card__row cut-text">
 						<div className="card__parameter">Created:</div>
 						<div className="card__value">{created}</div>
 					</div>
-					<div className="card__row">
+					<div className="card__row cut-text">
 						<div className="card__parameter">Location:</div>
 						<div className="card__value">{location}</div>
 					</div>
